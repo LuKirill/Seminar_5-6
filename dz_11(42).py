08cf98d8fc7853a711139cd91f5a63ed2d1455bd
LuKirill <kirill-mihalich@mail.ru>
2022-09-01 17:38:37 +0300

    second commit

---
+++ dz_11(42).py
@@ -0,0 +1,38 @@
+# Реализовать RLE алгоритм. реализовать модуль сжатия и восстановления данных.
+# a. входные и выходные данные хранятся в отдельных текстовых файлах
+
+def rle_encode(string):
+    encoding = ''
+    prev_char = ''
+    count = 1
+    for char in string:
+        if char != prev_char:
+            if prev_char:
+                encoding += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    else:
+        encoding += str(count) + prev_char
+        return encoding
+en = rle_encode("rrrrrrrrrrttttttttttyyyyyyyy")
+print(en)
+
+def rle_decode(string):
+    decode = ''
+    count = ''
+    for char in string:
+        if char.isdigit():
+            count += char
+        else:
+            decode += char * int(count)
+            count = ''
+    return decode
+de = rle_decode("10r10t8y")
+print(de)
+
+with open('rle_encode.txt', 'w', encoding='utf-8') as encode:
+    encode.write(en)
+with open('rle_decode.txt', 'w', encoding='utf-8') as decode:
+    decode.write(de)
--- dz_7(38).py
+++ dz_7(38).py
@@ -1,5 +1,7 @@
 # Напишите программу, удаляющую из текста все слова содержащие "абв".
 
-my_text = input("")
-res_my_text = my_text.replace("абв", "")
-print(res_my_text)
+my_text = "f,df вьывль львыа абв абвапро a,d абв фффабвсс"
+my_text = my_text.split()
+new_text = [i for i in my_text if "абв" in i]
+new_text = [j for j in my_text if j not in new_text]
+print(new_text)
